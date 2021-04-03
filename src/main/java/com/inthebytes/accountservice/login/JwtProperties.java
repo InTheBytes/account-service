9c0c672d3242239e4e2988b643097ddeededd846
lexinelson <lexi.nelson@smoothstack.com>
2021-04-02 19:10:30 -0700

    ITB-7 Hid & Changed JWT Secret

--- src/main/java/com/inthebytes/accountservice/login/JwtProperties.java
+++ src/main/java/com/inthebytes/accountservice/login/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.inthebytes.accountservice.login;
 
 public class JwtProperties {
-	public static final String SECRET = "StackLunchBunch";
+	public static final String SECRET = System.getenv("SL_SECRET");
 	public static final int EXPIRATION_TIME = 3600000 * 2; //2 hours
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authentication";
