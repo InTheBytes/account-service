eeb60ebc4c012c29fd9bd1c74b55a51255acd474
Michael-Acker <michaelacker11@gmail.com>
2021-04-08 11:51:30 -0400

    Added permissions to security config

--- src/main/java/com/inthebytes/accountservice/SecurityConfig.java
+++ src/main/java/com/inthebytes/accountservice/SecurityConfig.java
@@ -1,18 +1,30 @@
 package com.inthebytes.accountservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-        security.httpBasic().disable()
-            .csrf().disable(); // TODO: REMOVE ON PROD
+    protected void configure(HttpSecurity security) throws Exception {
+    	//TODO: Actively populate with restrictions
+    	security.authorizeRequests().antMatchers("/public").permitAll()
+        .antMatchers("/authticated").authenticated()
+        .antMatchers("/admin").hasRole("ADMIN")
+        .antMatchers("/user").hasAnyRole("ADMIN","USER")
+        .and().httpBasic();
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
 }
