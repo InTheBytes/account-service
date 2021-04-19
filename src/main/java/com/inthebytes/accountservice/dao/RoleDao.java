5a6477d2db31df315269270f2a36edc3476d8409
Michael-Acker <michaelacker11@gmail.com>
2021-04-19 06:51:09 -0400

    Testing

--- src/main/java/com/inthebytes/accountservice/dao/RoleDao.java
+++ src/main/java/com/inthebytes/accountservice/dao/RoleDao.java
@@ -1,9 +1,11 @@
 package com.inthebytes.accountservice.dao;
 
-import com.inthebytes.accountservice.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.inthebytes.accountservice.model.UserRole;
+
 @Repository
-public interface RoleDao extends JpaRepository<Role, Long> {
+public interface RoleDAO extends JpaRepository<UserRole, Long> {
+	UserRole findRoleByRoleId(Integer id);
 }
