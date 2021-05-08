24dbdc22cc05db8781f339cdd1ce009481a0bb95
lexinelson <lexi.nelson@smoothstack.com>
2021-05-07 21:18:12 -0700

    ITB-88: Created mapper service for User Dtos

--- src/main/java/com/inthebytes/accountservice/dto/UserDto.java
+++ src/main/java/com/inthebytes/accountservice/dto/UserDto.java
@@ -1,19 +1,152 @@
 package com.inthebytes.accountservice.dto;
 
+import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.NonNull;
+
+import com.sun.istack.Nullable;
+
 public class UserDto {
+	
+	@Id
+	private Long userId;
+	
+	@NonNull
+	private RoleDto role;
+	
+	@NonNull
 	private String username;
+	
+	@Nullable
 	private String password;
+	
+	@NonNull
+	@Pattern(regexp="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+	private String email;
+	
+	@Nullable
+	@Pattern(regexp="^[0-9-]+$")
+	@Size(max=11, min=7)
+	private String phone;
+	
+	@Nullable
+	private String firstName;
+	
+	@Nullable
+	private String lastName;
+	
+	@NonNull
+	private Boolean isActive;
+	
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public RoleDto getRole() {
+		return role;
+	}
+
+	public void setRole(RoleDto role) {
+		this.role = role;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDto other = (UserDto) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDto [userId=" + userId + ", role=" + role + ", username=" + username + ", email=" + email
+				+ ", isActive=" + isActive + "]";
 	}
 	
 }
---
+++ src/main/java/com/inthebytes/accountservice/mapper/UserMapper.java
@@ -0,0 +1,54 @@
+package com.inthebytes.accountservice.mapper;
+
+import org.springframework.stereotype.Service;
+
+import com.inthebytes.accountservice.dto.RoleDto;
+import com.inthebytes.accountservice.dto.UserDto;
+import com.inthebytes.accountservice.entity.Role;
+import com.inthebytes.accountservice.entity.User;
+
+@Service
+public class UserMapper {
+
+	public UserDto convert(User user) {
+		UserDto result = new UserDto();
+		result.setUserId(user.getUserId());
+		result.setUsername(user.getUsername());
+		result.setPassword(user.getPassword());
+		result.setEmail(user.getEmail());
+		result.setPhone(user.getPhone());
+		result.setIsActive(user.getActive());
+		result.setFirstName(user.getFirstName());
+		result.setLastName(user.getLastName());
+		result.setRole(convert(user.getRole()));
+		return null;
+	}
+	
+	public User convert(UserDto user) {
+		User result = new User();
+		result.setUserId(user.getUserId());
+		result.setUsername(user.getUsername());
+		result.setPassword(user.getPassword());
+		result.setEmail(user.getEmail());
+		result.setPhone(user.getPhone());
+		result.setActive(user.getIsActive());
+		result.setFirstName(user.getFirstName());
+		result.setLastName(user.getLastName());
+		result.setRole(convert(user.getRole()));
+		return null;
+	}
+	
+	public RoleDto convert(Role role) {
+		RoleDto result = new RoleDto();
+		result.setRoleId(role.getRoleId());
+		result.setName(role.getName());
+		return result;
+	}
+	
+	public Role convert(RoleDto role) {
+		Role result = new Role();
+		result.setRoleId(role.getRoleId());
+		result.setName(role.getName());
+		return result;
+	}
+}
